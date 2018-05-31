0274de65716dd5abf4a64e11ef79fde3682b1e4b
Dhananjay131 <djwid92@gmail.com>
2018-05-30 18:12:45 -0700

    Add files via upload

---
+++ Puppy.java
@@ -0,0 +1,10 @@
+public class Puppy{
+  public Puppy(String s){
+    System.out.println("printing in constructor: "+ s);
+  }
+
+  public static void main(String[] args){
+    Puppy puppy = new Puppy("HELLO");
+
+  }
+}
