70322523917ec40dd101e981a8d7ab635866fcbb
Dhananjay131 <djwid92@gmail.com>
2018-05-30 20:35:28 -0700

    Add files via upload

--- Loops.java
+++ Loops.java
@@ -2,6 +2,8 @@ public class Loops{
   public static void main(String[] args){
     int[] arr = {1,2,3,4,6,43};
 
+    System.out.printf("hello %f hello %d Hello %s", 11.02,23,"LOL!"+ "\n");
+
     String[] str = {"aa", "bb", "cc", "dd" };
 
     for(int i : arr){
