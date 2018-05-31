ba2b704368978a15f0283a54a33b3f2a0854ce90
Dhananjay131 <djwid92@gmail.com>
2018-05-30 18:23:00 -0700

    Add files via upload

--- Puppy.java
+++ Puppy.java
@@ -1,10 +1,28 @@
 public class Puppy{
+  public int puppyAge;
+
+  public Puppy(){
+    System.out.println("default constructor ");
+  }
+
   public Puppy(String s){
     System.out.println("printing in constructor: "+ s);
   }
 
+  public void setAge(int num){
+    puppyAge = num;
+  }
+
+  public int getAge(){
+    System.out.println("inside getter- "+ puppyAge);
+    return puppyAge;
+  }
   public static void main(String[] args){
-    Puppy puppy = new Puppy("HELLO");
+    Puppy puppy = new Puppy("Puppy1");
+
+    Puppy puppy2 = new Puppy();
+    puppy2.setAge(3);
+    System.out.println("getting method- "+ puppy2.getAge());
 
   }
 }
